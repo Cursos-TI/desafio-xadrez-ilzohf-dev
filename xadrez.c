a05802928e667927bb557ed0818f545fbcd217c4
ilzohf-dev <ilzo.hf@gmail.com>
2026-03-06 18:19:47 +0000

    loops simples finalizados

--- xadrez.c
+++ xadrez.c
@@ -5,21 +5,45 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
     // Nível Novato - Movimentação das Peças
-    // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
+
+    int torre = 1, bispo = 1, rainha = 1, cavalo = 1;
 
     // Implementação de Movimentação do Bispo
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    
+    do {
+            printf("movendo Bispo %d casa para cima/direita.\n", bispo);
+            bispo++;
+        } while (bispo <= 5);
+        printf("Aguarde a proxima jogada...\n");
 
     // Implementação de Movimentação da Torre
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
+    
+    while (torre <= 5)
+            {
+                printf("movendo torre %d casa para direita.\n", torre);
+                torre++;
+            }
+        printf("Aguarde a proxima jogada...\n");
 
     // Implementação de Movimentação da Rainha
-    // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    
+    for (rainha = 1; rainha <= 8; rainha++) {
+            printf("movendo Rainha %d Casa para esquerda.\n", rainha);
+        }
+        printf("Aguarde a proxima jogada...\n");
 
     // Nível Aventureiro - Movimentação do Cavalo
-    // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
-    // Um loop pode representar a movimentação horizontal e outro vertical.
+    
+    while (cavalo--)
+        {
+            for (int c = 1; c < 3; c++)
+            {
+                printf("Cavalo %d casa a baixo\n", c);
+            }
+            printf("Cavalo casa a esquerda\n");
+        }
 
     // Nível Mestre - Funções Recursivas e Loops Aninhados
     // Sugestão: Substitua as movimentações das peças por funções recursivas.
